a4d2e9a4d8f093fd53a88b52cc8f4ca2cab3cd40
sharker92 <sharker92@hotmail.com>
2017-07-06 13:55:00 -0600

    Icono y segundo Activity
    Se le agrego el icono de sharks en la aplicación y en el action bar, un segundo activity asi como el boton para cambiar entre activities y el boton de retorno al Main.
    Se le agrego otro boton y un text view para usos futuros.

---
+++ app/src/main/java/com/sharkstech/dolphin/Beacons.java
@@ -0,0 +1,85 @@
+package com.sharkstech.dolphin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.kontakt.sdk.android.ble.connection.OnServiceReadyListener;
+import com.kontakt.sdk.android.ble.manager.ProximityManager;
+import com.kontakt.sdk.android.ble.manager.ProximityManagerFactory;
+import com.kontakt.sdk.android.ble.manager.listeners.EddystoneListener;
+import com.kontakt.sdk.android.ble.manager.listeners.IBeaconListener;
+import com.kontakt.sdk.android.ble.manager.listeners.simple.SimpleEddystoneListener;
+import com.kontakt.sdk.android.ble.manager.listeners.simple.SimpleIBeaconListener;
+import com.kontakt.sdk.android.common.KontaktSDK;
+import com.kontakt.sdk.android.common.profile.IBeaconDevice;
+import com.kontakt.sdk.android.common.profile.IBeaconRegion;
+import com.kontakt.sdk.android.common.profile.IEddystoneDevice;
+import com.kontakt.sdk.android.common.profile.IEddystoneNamespace;
+
+import static com.sharkstech.dolphin.R.layout.activity_beacons;
+
+public class Beacons extends AppCompatActivity {
+
+    private ProximityManager proximityManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_beacons);
+        //Activar flecha de atras
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+        //Mi Clave API
+        KontaktSDK.initialize("amwGefsIOPirddbsEWkXusNXUTIEdnAD");
+/*
+        proximityManager = ProximityManagerFactory.create(this);
+        proximityManager.setIBeaconListener(createIBeaconListener());
+        proximityManager.setEddystoneListener(createEddystoneListener());*/
+    }
+/*
+    @Override
+    protected void onStart() {
+        super.onStart();
+        startScanning();
+    }
+
+    @Override
+    protected void onStop() {
+        proximityManager.stopScanning();
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        proximityManager.disconnect();
+        proximityManager = null;
+        super.onDestroy();
+    }
+
+    private void startScanning() {
+        proximityManager.connect(new OnServiceReadyListener() {
+            @Override
+            public void onServiceReady() {
+                proximityManager.startScanning();
+            }
+        });
+    }
+
+    private IBeaconListener createIBeaconListener() {
+        return new SimpleIBeaconListener() {
+            @Override
+            public void onIBeaconDiscovered(IBeaconDevice ibeacon, IBeaconRegion region) {
+                Log.i("Sample", "IBeacon discovered: " + ibeacon.toString());
+            }
+        };
+    }
+
+    private EddystoneListener createEddystoneListener() {
+        return new SimpleEddystoneListener() {
+            @Override
+            public void onEddystoneDiscovered(IEddystoneDevice eddystone, IEddystoneNamespace namespace) {
+                Log.i("Sample", "Eddystone discovered: " + eddystone.toString());
+            }
+        };
+    }*/
+}
--- app/src/main/java/com/sharkstech/dolphin/MainActivity.java
+++ app/src/main/java/com/sharkstech/dolphin/MainActivity.java
@@ -1,13 +1,40 @@
 package com.sharkstech.dolphin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.kontakt.sdk.android.ble.manager.ProximityManager;
+
+import com.kontakt.sdk.android.common.KontaktSDK;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ProximityManager proximityManager;
+    private Button btn;
+    private String GREETER = "Que rollo pollo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Mostrar Icono de la aplicación
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.ic_sharksicon);
+        //Boton Beacons Activity
+        btn = (Button) findViewById(R.id.buttonBeacons);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+           @Override
+            public void onClick(View view){
+               //Acceder al beacon activity y enviarle un string
+               Intent intent = new Intent(MainActivity.this, Beacons.class);
+               startActivity(intent);
+           }
+        });
+
     }
 }
